af674515eebc701cbe34e8bee898cdaaa3c7e34b
ellometa <kumaraayush471@gmail.com>
2025-04-30 22:36:27 +0530

    yes

--- zBuffer/jigyasu_template.cpp
+++ zBuffer/jigyasu_template.cpp
@@ -1,23 +1,18 @@
 // ============================================================================ //
-// ||------------------------------------------------------------------------|| //
-// ||------------------------------------------------------------------------|| //
-//*||--------------------------//GanduJi's Template//------------------------|| //
-// ||-------------------------Expert till August 2025------------------------|| //
-// ||------------------------------------------------------------------------|| //
-// ||------------------------------------------------------------------------|| //
+// ||                                                                        || //
+// ||                                                                        || //
+//*||                     // Jigyasu's Template for cpp //                   || //
+// ||--------------------------Expert till July 2025-------------------------|| //
+// ||                                                                        || //
+// ||                                                                        || //
 // ============================================================================ //
 
-// CSI SE BADLA LUNGA MAI 
-// thank you jigyasu kalyan bhai
-// Last year to achieve my goals, pray_emoji
-
-
 #include <bits/stdc++.h>
-
 using namespace std;
 
-#define fast_input() ios::sync_with_stdio(false); cin.tie(nullptr);
-#define int long long
+#define fast_io cin.sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define int long long int
+#define ll long long
 #define pb push_back
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
@@ -26,19 +21,20 @@ using namespace std;
 #define s second
 #define MOD 1000000007
 #define endl "\n"
+#define IN(a, n) vi a(n); fr(i, 0, n) cin >> a[i];
+#define yes cout << "YES" << endl;
+#define no cout << "NO" << endl;
 
 #define vi vector<int>
 
 #define fr(i, a, b) for (int i = a; i < b; i++)
 #define frr(i, a, b) for (int i = a; i > b; i--)
 
-#define IN(a, n) vi a(n); fr(i, 0, n) cin >> a[i];
-
 //?--------------------------------------------------------------------------------------------------------------------
- 
+
 //* Debuggers
 #define debug(x) cout << #x << " = " << x << endl;
- 
+
 template<typename T>
 void print_container(const T& container) {
     for (auto it = container.begin(); it != container.end(); ++it) {
@@ -48,13 +44,9 @@ void print_container(const T& container) {
     cout << endl;
 }
 #define print(x) print_container(x);
- 
-//--------------------------------------------------------------------------------------------------------------------
 
-const int INF = LLONG_MAX >> 1;
+//?--------------------------------------------------------------------------------------------------------------------
 
-//--------------------------------------------------------------------------------------------------------------------
- 
 //* GCD using the Euclidean Algorithm
 template <typename T>
 T gcd(T a, T b) {
@@ -64,11 +56,11 @@ T gcd(T a, T b) {
     }
     return a;
 }
- 
+
 //* LCM using the GCD formula
 template <typename T>
 T lcm(T a, T b) {return (a / gcd(a, b)) * b;}  // a * b / gcd(a, b)
- 
+
 //* Sieve of Eratosthenes for primes
 vector<int> sieve(int n) {
     vi st;
@@ -88,67 +80,90 @@ vector<int> sieve(int n) {
     }
     return st;
 }
- 
-//--------------------------------------------------------------------------------------------------------------------
+
+//?--------------------------------------------------------------------------------------------------------------------
 
 //* Modular Multiplication
-int mod_mul(int a, int b) {return ((a % MOD) * (b % MOD)) % MOD;}
- 
+ll mod_mul(ll a, ll b) {return ((a % MOD) * (b % MOD)) % MOD;}
+
 //* Binary Exponentiation
-int binpow(int a, int b, int m = MOD) {
-    int res = 1;
+ll binpow(ll a, ll b, ll m = MOD) {
+    ll res = 1;
     a %= m;
     while (b) {
-        if (b & 1LL) res = res * a % m;
+        if (b & 1ll) res = res * a % m;
         a = mod_mul(a, a);
-        b >>= 1LL;
+        b >>= 1ll;
     }
     return res;
 }
- 
+
 //* Modular Addition
-int mod_add(int a, int b) {return ((a % MOD) + (b % MOD)) % MOD;}
- 
+ll mod_add(ll a, ll b) {return ((a % MOD) + (b % MOD)) % MOD;}
+
 //* Modular Subtraction
-int mod_sub(int a, int b) {return ((a % MOD) - (b % MOD) + MOD) % MOD;}
- 
+ll mod_sub(ll a, ll b) {return ((a % MOD) - (b % MOD) + MOD) % MOD;}
+
 //* Modular Division (using Modular Inverse)
-int mod_inv(int a, int m = MOD) {return binpow(a, m - 2, m);}  // Fermat's Little Theorem
- 
-int mod_div(int a, int b) {return mod_mul(a, mod_inv(b));}
- 
+ll mod_inv(ll a, ll m = MOD) {return binpow(a, m - 2, m);}  // Fermat's Little Theorem
+
+ll mod_div(ll a, ll b) {return mod_mul(a, mod_inv(b));}
+
 //?--------------------------------------------------------------------------------------------------------------------
 
 //! Factorial with modular arithmetic
-vector<int> fact, inv_fact;
-void init_factorial(int n, int m = MOD) {
+vector<long long> fact, inv_fact;
+void init_factorial(int n, ll m = MOD) {
     fact.resize(n + 1, 1);
     inv_fact.resize(n + 1, 1);
     for (int i = 2; i <= n; ++i) fact[i] = fact[i - 1] * i % m;
     inv_fact[n] = binpow(fact[n], m - 2, m); // Fermat's Little Theorem for modular inverse
     for (int i = n - 1; i >= 1; --i) inv_fact[i] = inv_fact[i + 1] * (i + 1) % m;
 }
-int nCr(int n, int r, int m = MOD) {
+long long nCr(int n, int r, ll m = MOD) {
     if (r > n) return 0;
     return fact[n] * inv_fact[r] % m * inv_fact[n - r] % m;
 }
 
 //?--------------------------------------------------------------------------------------------------------------------
+//?--------------------------------------------------------------------------------------------------------------------
 
-void Solve(){
+// vi primes;
+
+void Solve() {
     int n; cin >> n;
-    cout << n+1;
+    IN(vec, n);
+    vi ans;
+    ans.pb(0);
+    for (int i = 1; i < n; i++) {
+        bitset<32>temp1(ans.back() ^ vec[i-1]);
+        bitset<32>temp2(vec[i]);
+        string temp = "";
+        for (int i = 31; i >= 0; i--) {
+            if (!temp2[i] && temp1[i]) temp += "1";
+            else temp += "0";
+        }
+        // reverse(all(temp));
+        // cout << temp1 << endl;
+        // cout << temp2 << endl;
+        // cout << temp << endl;
+        int hi = stoll(temp, 0, 2);
+        ans.pb(hi);
+    }
+    print(ans);
 }
 
-signed main(){
-    fast_input();
+int32_t main() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
 
-    int t=1;
-    // cin >> t;
-    for (int i = 1; i <= t; i++){
-        // cout << "Case " << i << ":" newline;
-        Solve();
-    }
+    // primes = sieve(1e7);
 
-    return 0;
+    fast_io;
+
+    int t = 1;
+    cin >> t;
+    while (t--) Solve();
 }
