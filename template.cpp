ae20dd678b873d0f8e7c31f207fc6b664e01110e
ellometa <kumaraayush471@gmail.com>
2025-05-21 23:42:16 +0530

    Comeback.... maybe

--- template.cpp
+++ template.cpp
@@ -1,14 +1,12 @@
 // ============================================================================ //
 // ||------------------------------------------------------------------------|| //
 // ||------------------------------------------------------------------------|| //
-//*||--------------------------//GanduJi's Template//------------------------|| //
-// ||-------------------------Expert till August 2025------------------------|| //
+//*||----------------------//EmasculatorJi's Template//----------------------|| //
+// ||---------------------------Expert till Dec 2025-------------------------|| //
 // ||------------------------------------------------------------------------|| //
 // ||------------------------------------------------------------------------|| //
 // ============================================================================ //
 
-// CSI SE BADLA LUNGA MAI 
-// thank you jigyasu kalyan bhai
 // Last year to achieve my goals, pray_emoji
 
 
@@ -19,6 +17,7 @@ using namespace std;
 #define fast_input() ios::sync_with_stdio(false); cin.tie(nullptr);
 #define int long long
 #define pb push_back
+#define nline <<'\n'
 #define all(v) v.begin(), v.end()
 #define rall(v) v.rbegin(), v.rend()
 #define sz(x) (int)(x).size()
@@ -35,10 +34,11 @@ using namespace std;
 #define IN(a, n) vi a(n); fr(i, 0, n) cin >> a[i];
 
 //?--------------------------------------------------------------------------------------------------------------------
- 
+
 //* Debuggers
 #define debug(x) cout << #x << " = " << x << endl;
- 
+
+
 template<typename T>
 void print_container(const T& container) {
     for (auto it = container.begin(); it != container.end(); ++it) {
@@ -48,13 +48,13 @@ void print_container(const T& container) {
     cout << endl;
 }
 #define print(x) print_container(x);
- 
+
 //--------------------------------------------------------------------------------------------------------------------
 
 const int INF = LLONG_MAX >> 1;
 
 //--------------------------------------------------------------------------------------------------------------------
- 
+
 //* GCD using the Euclidean Algorithm
 template <typename T>
 T gcd(T a, T b) {
@@ -64,11 +64,11 @@ T gcd(T a, T b) {
     }
     return a;
 }
- 
+
 //* LCM using the GCD formula
 template <typename T>
 T lcm(T a, T b) {return (a / gcd(a, b)) * b;}  // a * b / gcd(a, b)
- 
+
 //* Sieve of Eratosthenes for primes
 vector<int> sieve(int n) {
     vi st;
@@ -88,12 +88,12 @@ vector<int> sieve(int n) {
     }
     return st;
 }
- 
-//--------------------------------------------------------------------------------------------------------------------
+
+//---------------------------------------------------------------------------------------------------------
 
 //* Modular Multiplication
 int mod_mul(int a, int b) {return ((a % MOD) * (b % MOD)) % MOD;}
- 
+
 //* Binary Exponentiation
 int binpow(int a, int b, int m = MOD) {
     int res = 1;
@@ -117,7 +117,7 @@ int mod_inv(int a, int m = MOD) {return binpow(a, m - 2, m);}  // Fermat's Littl
  
 int mod_div(int a, int b) {return mod_mul(a, mod_inv(b));}
  
-//?--------------------------------------------------------------------------------------------------------------------
+//?----------------------------------------------------------------------------------------------------------
 
 //! Factorial with modular arithmetic
 vector<int> fact, inv_fact;
@@ -133,11 +133,12 @@ int nCr(int n, int r, int m = MOD) {
     return fact[n] * inv_fact[r] % m * inv_fact[n - r] % m;
 }
 
-//?--------------------------------------------------------------------------------------------------------------------
+//?-----------------------------------------------------------------------------------------------------------
 
 void Solve(){
     int n; cin >> n;
     cout << n+1;
+    
 }
 
 signed main(){
@@ -146,7 +147,7 @@ signed main(){
     int t=1;
     // cin >> t;
     for (int i = 1; i <= t; i++){
-        // cout << "Case " << i << ":" newline;
+        // cout << "Case " << i << ":" << endl;
         Solve();
     }
 
