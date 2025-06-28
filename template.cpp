8960b5f6e698dcc2e19d6575a36bcf266506b6e7
ellometa <kumaraayush471@gmail.com>
2025-06-28 17:12:58 +0530

    Daily Update

--- template.cpp
+++ template.cpp
@@ -1,24 +1,51 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
 using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+mt19937_64 RNG(chrono::steady_clock::now().time_since_epoch().count());
 
 #define fast_input() ios::sync_with_stdio(false); cin.tie(nullptr);
+
 #define int long long
+#define fr(i, a, b) for (int i = (a); i < (int)(b); ++i)
+#define frr(i, a, b) for (int i = (a); i > (int)(b); --i)
+#define ina(x, n)               \
+    for (int i = 0; i < n; ++i) \
+        cin >> x[i];
+#define in(n)    \
+    long long n; \
+    cin >> n;
+#define double long double
 #define pb push_back
-#define nline <<'\n'
-#define all(v) v.begin(), v.end()
-#define rall(v) v.rbegin(), v.rend()
-#define sz(x) (int)(x).size()
-#define f first
-#define s second
+#define vi vector<int>
+#define mii map<int, int>
+#define vvi vector<vector<int>>
+#define vpi vector<pair<int, int>>
+#define mdi map<double, int>
+#define mci map<char, int>
+#define mide map<int, deque<int>>
+#define pi pair<int, int>
+#define si set<int>
+
+
+#define ff first
+#define ss second
 #define MOD 1000000007
 #define endl "\n"
+#define oyes cout << "YES" << endl;
+#define ono cout << "NO" << endl;
+#define oyess cout << "Yes" << endl;
+#define onoo cout << "No" << endl;
+#define ve1 cout << "-1" << endl;
 
 #define vi vector<int>
+#define all(x) (x).begin(), (x).end()
+#define rall(x) reverse((x).begin(), (x).end())
+#define sz(x) (int)(x).size()
 
-#define fr(i,a,b) for(int i = a; i < b; i++)
-#define frr(i,a,b) for(int i = a; i > b; i--)
-
-#define IN(a, n) vi a(n); fr(i, 0, n) cin >> a[i];
+#define nline <<'\n'
 
 const int INF = LLONG_MAX >> 1;
 
@@ -39,10 +66,11 @@ void print_container(const T& container) {
 #define print(x) print_container(x);
 
 
+
 //?-----------------------------------------------------------------------------------------------------------
 
 void solve(){
-    int n; cin >> n;
+    in(n);
     
 }
 
