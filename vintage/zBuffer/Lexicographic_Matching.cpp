31a64d04150cc9ad4d897c6c5a28d6d14cf86115
ellometa <kumaraayush471@gmail.com>
2025-10-31 00:02:17 +0530

    Happy Birthday

---
+++ vintage/zBuffer/Lexicographic_Matching.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+
+#define nline <<'\n' 
+
+
+using namespace std;
+
+
+void solve(){
+
+}
+
+signed main(){
+
+    int t = 1;
+    cin >> t;
+    for (int i = 1; i <= t; i++){
+        solve();
+        cout << "Case # " << i <<  " :" nline;
+    }
+}
+
