758715ceccdcda05346ac1c62fcf9608964e1da4
ellometa <kumaraayush471@gmail.com>
2026-05-21 12:15:50 +0530

    Reorganize stray root files into contest folders
    Move loose Codeforces and AtCoder problem files from the repo root into
    their respective contest subfolders, matching the existing layout.

---
+++ AtCoder/abc453/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mod 1000000007
+#define vi vector<int>
+#define pii pair<int,int>
+
+void solve() {
+    
+}
+
+int main() {
+
+    ll test = 1;
+    cin >> test;
+    while (test--) {
+        solve();
+    }
+    return 0;
+}
